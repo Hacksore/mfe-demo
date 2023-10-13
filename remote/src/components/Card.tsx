53eecdfc2df25fcb67dca878c6931622cfadbca8
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-12 20:57:49 -0500

    Ship

--- remote/src/components/Card.tsx
+++ remote/src/components/Card.tsx
@@ -1,9 +1,13 @@
 const Nav = () => {
   return (
-    <div
-      style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
-    >
-      This is a demo of a card
+    <div style={{ width: 200, height: 200, backgroundColor: "lightblue" }}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
     </div>
   );
 };
