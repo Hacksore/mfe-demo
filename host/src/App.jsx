be39704d83fafb97fcb29244cb8efeb5a70391d1
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-12 20:55:32 -0500

    Shipit

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -6,13 +6,17 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <h1>Vite + React MFE Demo</h1>
+      <h1 className="">MFE Home</h1>
 
-      {Array(10)
-        .fill(0)
-        .map(() => (
-          <Card />
-        ))}
+      {/* make a tailwind grid of card */}
+      <div className="flex flex-wrap gap-2">
+
+        {Array(10)
+          .fill(0)
+          .map(() => (
+            <Card />
+          ))}
+      </div>
     </div>
   );
 }
